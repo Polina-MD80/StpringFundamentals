4e62f2d8477e10d4be555e30af257068c25ed8b0
Polina Pavlova <poli.paskaleva@gmail.com>
2021-10-11 18:22:44 +0000

    about controller added

---
+++ pathfinder/src/main/java/softuni/bg/pathfinder/web/AboutController.java
@@ -0,0 +1,12 @@
+package softuni.bg.pathfinder.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+    @GetMapping("/about")
+    private String about(){
+        return "about";
+    }
+}
